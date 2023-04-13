4587fa496b9fd5db381e88ec113e641f0c0d4b2b
Xerkse <cjmp9876@gmail.com>
2023-04-13 10:23:50 -0500

    adding new line to my init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua
 -- :Lazy for plugins
---
+-- 
+-- adding this to see if things are working with git
 require "plugins"
 require "theme"
 require "general"
