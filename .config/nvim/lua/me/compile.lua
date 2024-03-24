ec90f9b0fd657332f181c77dd6bb64f8c30b729a
Xerkse <cjmp9876@gmail.com>
2024-03-24 10:23:45 -0500

    nvim things

--- .config/nvim/lua/me/compile.lua
+++
@@ -1,3 +0,0 @@
-  -- compile keymaps
-  vim.keymap.set('n', '<leader><F12>',':w!<CR>:!compiler %<CR>', { desc = 'Compile show' })
-  vim.keymap.set('n', '<F12>',':w!<CR>:silent exec "!compiler %"<CR>', { desc = 'Compile silent' })
