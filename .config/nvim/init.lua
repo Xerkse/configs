028585b5084d20504b766d08872a1d2585addafd
Xerkse <cjmp9876@gmail.com>
2024-04-17 12:44:38 -0500

    more things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 -- GENERAL
 -- Set <space> as the leader key
 -- See `:help mapleader`
---  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+-- NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
