5b0163601f731836dd538963f5f0d9bee852aa4c
Xerkse <cjmp9876@gmail.com>
2025-09-28 20:29:02 -0500

    mark script, so like nsxiv but for muisc/vid

---
+++ .config/mpv/scripts/mark.lua
@@ -0,0 +1,42 @@
+-- local mp = require 'mp.utils'
+local marked = {}
+
+function mark_current()
+    local path = mp.get_property("path")
+    if path then
+        if not marked[path] then
+            marked[path] = true
+            print("Marked: " .. path)
+        else
+            marked[path] = nil
+            print("Unmarked: " .. path)
+        end
+    end
+end
+
+function write_marks()
+    local out = {}
+    for k, _ in pairs(marked) do
+        table.insert(out, k)
+    end
+
+    local f, err = io.open("/tmp/mpv_marked.txt", "w")
+	if not f then
+        print("Error opening file for writing: " .. (err or "unknown"))
+        return
+	end
+
+	f:write(table.concat(out, "\n"))
+	f:close()
+end
+
+local marked = false
+function enable_marks()
+    if marked == false then
+        mp.add_forced_key_binding("m", "mark-current", mark_current)
+        mp.register_event("shutdown", write_marks)
+    end
+end
+
+mp.add_key_binding("Alt+m", "enable_marks", enable_marks)
+
